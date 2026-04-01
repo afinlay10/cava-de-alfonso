dbdba108e1c84964201222f329a8781d41709de1
afinlay10 <alfonso.finlay@gmail.com>
2026-04-01 13:15:22 -0300

    Delete cava-app directory

--- cava-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CavaDeVinos from './CavaDeVinos'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CavaDeVinos />
-  </React.StrictMode>
-)
